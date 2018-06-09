4dad373cd7059ab4b730ef24175e178d73bffc46
Syed Sana ul Haq Fazli <syedfazli@hotmail.com>
2018-06-09 16:17:47 +1200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,48 @@
+
+/*
+ * MEGA web Development Test
+ *
+ * Presented By: Syed Sana ul Haq Fazli
+ * http://www.syedfazli.com
+ *
+ * Source Help From:
+ * https://github.com/blueimp/jQuery-File-Upload
+ */
+/* global $, window */
+
+$(function () {
+    'use strict';
+
+    // Initialize the jQuery File Upload widget:
+    $('#fileupload').fileupload({
+        // Uncomment the following to send cross-domain cookies:
+        //xhrFields: {withCredentials: true},
+        url: 'server/php/'
+    });
+
+    // Enable iframe cross-domain access via redirect option:
+    $('#fileupload').fileupload(
+        'option',
+        'redirect',
+        window.location.href.replace(
+            /\/[^\/]*$/,
+            '/cors/result.html?%s'
+        )
+    );
+
+   
+    // Load existing files:
+    $('#fileupload').addClass('fileupload-processing');
+    $.ajax({
+        url: $('#fileupload').fileupload('option', 'url'),
+        dataType: 'json',
+        context: $('#fileupload')[0]
+    }).always(function () {
+        $(this).removeClass('fileupload-processing');
+    }).done(function (result) {
+        $(this).fileupload('option', 'done')
+            .call(this, $.Event('done'), {result: result});
+    });
+   
+
+});
